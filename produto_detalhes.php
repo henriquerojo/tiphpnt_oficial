1c9e7aeaee8577a7d46ac731672fa920f0923ec0
henrique rojo <henriquerojo10@gmail.com>
2023-02-03 09:06:56 -0300

    ajuste de nome da variavel

--- produto_detalhes.php
+++ produto_detalhes.php
@@ -1,7 +1,7 @@
 <?php 
 include 'conn/connect.php';
 $lista = $conn->query("select * from vw_tbprodutos where destaque_produto = 'Sim';");
-$row_destaque =  $lista->fetch_assoc();
+$row_destalhes =  $lista->fetch_assoc();
 $num_linhas = $lista->num_rows;
 
 ?>
@@ -18,24 +18,24 @@ $num_linhas = $lista->num_rows;
             <div class="row">
                     <div class="col-sm-6 col-md-4">
                         <div class="thumbnail">
-                            <a href="produto_detalhes.php?id_produto=<?php echo $row_destaque ['id_produto']?>">
-                                <img src="images/<?php echo $row_destaque ['imagem_produto']?>" class="img-responsive img-rounded">
+                            <a href="produto_detalhes.php?id_produto=<?php echo $row_detalhes ['id_produto']?>">
+                                <img src="images/<?php echo $row_detalhes ['imagem_produto']?>" class="img-responsive img-rounded">
                             </a>
                             <div class="caption text-right">
                                 <h3 class="text-danger">
-                                    <strong><?php echo $row_destaque ['descri_produto']?></strong>
+                                    <strong><?php echo $row_detalhes ['descri_produto']?></strong>
                                 </h3>
                                 <p class="text-warning">
-                                    <strong><?php echo $row_destaque ['rotulo_tipo']?></strong>
+                                    <strong><?php echo $row_detalhes ['rotulo_tipo']?></strong>
                                 </p>
                                 <p class="text-left">
-                                    <?php echo mb_strimwidth($row_destaque ['resumo_produto'],0,42,'...');?>
+                                    <?php echo mb_strimwidth($row_detalhes ['resumo_produto'],0,42,'...');?>
                                 </p>
                                 <p>
                                     <button class="btn btn-default disable" role="button" style="cursor: default">
-                                        <?php echo "R$ ".number_format($row_destaque['valor_produto'],2,",",".");?>
+                                        <?php echo "R$ ".number_format($row_detalhes['valor_produto'],2,",",".");?>
                                     </button>
-                                    <a href="produto_detalhes.php?id_produto=<?php echo $row_destaque['id_produto']?>">
+                                    <a href="produto_detalhes.php?id_produto=<?php echo $row_detalhes['id_produto']?>">
                                         <span class="hidden-xs">Saiba Mais...</span>
                                         <span class="hidden-xs glyphicon glyphicon-eye-open" aria-hidden="true"></span>
                                     </a>
