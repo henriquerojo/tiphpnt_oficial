99a7027550cbb2350631db62978c7423ac3a15f3
henrique <henriquerojo10@gmail.com>
2023-02-03 17:13:44 -0300

    adição de campos no inserindo produtos

--- admin/produtos_insere.php
+++ admin/produtos_insere.php
@@ -11,7 +11,7 @@ if ($_POST){
     }
     $id_tipo = $_POST['id_tipo_produto'];
     $destaque = $_POST['destaque_produto'];
-    $descri = $_POST['descri_produto'];
+    $resumo = $_POST['descri_produto'];
     $resumo = $_POST['resumo_produto'];
     $valor = $_POST['valor_produto'];
     $imagem = $_POST['imagem_produto']['name'];
@@ -19,10 +19,12 @@ if ($_POST){
     $insereProd = "INSERT INTO tbprodutos
                    (id_tipo_produto, destaque_produto, descri_produto, resumo_produto, valor_produto, imagem_produto)
                    VALUES
-                   ('$id_tipo','$destaque','$descri','$resumo','$valor','$imagem'); 
+                   ('$id_tipo','$destaque','$resumo','$resumo','$valor','$imagem'); 
                     ";
     $resultado = $conn->query($insereProd);
-    // após a gravação bem sucedida do produto, volta (atualiza) lista
+    
+}
+// após a gravação bem sucedida do produto, volta (atualiza) lista
     if(mysqli_insert_id($conn)){
         header('location: produtos_lista.php');
     }
@@ -32,7 +34,6 @@ if ($_POST){
     $lista_fk = $conn->query($consulta_fk);
     $row_fk = $lista_fk->fetch_assoc();
     $nlinhas = $lista_fk->num_rows;
-}
 
 ?>
 <!DOCTYPE html>
@@ -75,11 +76,86 @@ if ($_POST){
                                     <?php } while ($row_fk = $lista_fk -> fetch_assoc());?>
                                 </select>
                             </div>
+                            <label for="destaque_produto">Destaque:</label>
+                            <div class="input-group">
+                                <label for="destaque_produto_s" class="radio-inline">
+                                    <input type="radio" name="destaque_produto" id="destaque_produto" value="Sim">Sim
+                                </label>
+                                <label for="destaque_produto_n" class="radio-inline">
+                                    <input type="radio" name="destaque_produto" id="destaque_produto" value="Não" checked>Não
+                                </label>
+                            </div>
+                            <label for="descri_produto">Descrição:</label>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+                                </span>
+                                <input type="text" name="descri_produto" id="descri_produto" 
+                                class="form-control" placeholder="Digite a descrição do Produto"
+                                maxlength="100" required>
+                            </div>
+
+                            <label for="resumo_produto">Resumo:</label>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-list-out" aria-hidden="true"></span>
+                                </span>
+                                <textarea name="resumo_produto" id="resumo_produto"
+                                cols="30" rows="8"
+                                class="form-control" placeholder="Digite os detalhes do Produto"
+                                required></textarea>
+                            </div>
+
+                            <label for="valor_produto">Valor:</label>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+                                </span>
+                                <input type="number" name="valor_produto" id="valor_produto" 
+                                class="form-control" required min="0" step="0.01">
+                            </div>
+                            <label for="imagem_produto">Imagem:</label>
+                            <div class="input-group">
+                                <span class="input-group-addon">
+                                    <span class="glyphicon glyphicon-pictures" aria-hidden="true"></span>
+                                </span>
+                                <img src="" name="imagem" id="imagem" alt="" class="img-responsive">
+                                <input type="file" name="imagem_produto" id="imagem_produto" class="form-control" accept="image/*">
+                            </div>
+                            <br>
+                            <input type="submit" name="enviar" id="enviar" class="btn btn-danger btn-block" value="Cadastrar">
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </main>
+<!-- SCRIPT PARA IMAGEM -->
+<script>
+    document.getElementById("imagem_produto").onchange = function(){
+        var reader = new FileReader();
+        if(this.files[0].size>528385){
+            alert("A imagem deve ter no no máximo 500 KB");
+            $("#imagem").attr("src", "blank");
+            $("#imagem").hide();
+            $("#imagem_produto").wrap('<form>').closest('form').get(0).reset();
+            $("#imagem_produto").unwrap;
+            return false
+        }
+        if(this.files[0].type.indexOf("image") == -1){
+            alert("Formato Inválido, Escolha Uma Imagem!");
+            $("#imagem").attr("src", "blank");
+            $("#imagem").hide();
+            $("#imagem_produto").wrap('<form>').closest('form').get(0).reset();
+            $("#imagem_produto").unwrap;
+            return false
+        }
+        reader.onload = function(e){
+            document.getElementById("imagem").src = e.target.result
+            $("#imagem").show();
+        }
+        reader.readAsDataURL(this.files[0])
+    }
+</script>
 </body>
 </html>
