a1bfa9112236ce861c6dc5feb8ad0a26e15ae697
henrique rojo <henriquerojo10@gmail.com>
2023-02-09 15:22:42 -0300

    adição de um item bebidas e começo de lista tipos

--- admin/tipos_lista.php
+++ admin/tipos_lista.php
@@ -3,6 +3,7 @@ include 'acesso_com.php';
 include '../conn/connect.php';
 $lista = $conn->query('select * from tbtipos;');
 $row = $lista->fetch_assoc();
+$rows = $lista->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -13,7 +14,40 @@ $row = $lista->fetch_assoc();
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/estilo.css">
 </head>
-<body>
-    
+<body class="fundofixo">
+    <?php include "menu_adm.php"?>
+    <main class="container">
+        <h2 class="breadcrumb alert-danger">Lista de Tipos</h2>
+        <table class="table table-hover table-condensed tb-opacidade bg-danger">
+            <thead>
+                <th class="hidden">ID</th>
+                <th>ROTULO</th>
+                <th>SIGLA</th>
+                <th>
+                    <a href="tipos_insere.php" target="_self" class="btn btn-block btn-primary btn-xs" role="button">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                        <span class="hidden-xs">ADICIONAR</span>
+                    </a>
+                </th>
+            </thead>
+
+            <tbody>
+                <?php do{?>
+                    <tr>
+                        <td class="hidden">
+                            <?php echo $row['id_tipo'];?>
+                        </td>
+                        <td>
+                            <?php echo $row['rotulo_tipo'];?>
+                            <span class="visible-xs"></span>
+                            <span class="hidden-xs"></span>
+                        </td>
+                        <td>
+                            <?php echo $row['sigla_tipo'];?>
+                        </td>
+                        
+                    </tr>
+                <?php }while($row = $lista->fetch_assoc());?>
+            </tbody>
 </body>
 </html>
