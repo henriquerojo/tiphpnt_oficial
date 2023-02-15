aa61bdf40115aa5e0a24eb2358a95b8bab841a49
henrique <henriquerojo10@gmail.com>
2023-02-15 14:58:43 -0300

    teste para sincronização em máquinas diferentes

--- cliente/index.php
+++ cliente/index.php
@@ -1,3 +1,13 @@
-<h2>
-  <strong></strong><?php echo $_GET['cliente'];?>,</strong>  Bem Vindo a sua área de cliente
-</h2>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/estilo.css">
+  <title>Cliente</title>
+</head>
+<body>
+  
+</body>
+</html>
