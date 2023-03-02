bd4a7d70f1f3ac56bcf43aca84c93358ec030c48
henrique rojo <henriquerojo10@gmail.com>
2023-03-02 11:53:47 -0300

    codificação usuarios insere

--- admin/usuarios_insere.php
+++ admin/usuarios_insere.php
@@ -3,23 +3,23 @@ include 'acesso_com.php';
 include '../conn/connect.php';
 
 if ($_POST){
+    $id_tipo = $_POST['id_tipo'];
+    $sigla_tipo = $_POST['sigla_tipo'];
+    $rotulo_tipo = $_POST['rotulo_tipo'];
 
-    $id_usuario = $_POST['id_usuario'];
-    $login_usuario = $_POST['login_usuario'];
-    $senha_usuario = $_POST['senha_usuario'];
-    $nivel_usuario = $_POST['resumo_produto'];
-
-    $insereUser = "INSERT INTO tbusuarios
-                   (id_usuario, login_usuario, senha_usuario, nivel_usuario)
-                   VALUES
-                   ('$id_usuario','$login_usuario','$senha_usuario','$senha_usuario','$nivel_usuario');";
-    $resultado = $conn->query($insereUser);
-    
+    $insereTipos = "INSERT INTO tbtipos
+                (id_tipo, sigla_tipo, rotulo_tipo)
+                VALUES
+                ('$id_tipo','$sigla_tipo','$rotulo_tipo');";
+    $resultado = $conn->query($insereTipos);
 }
-// após a gravação bem sucedida do produto, volta (atualiza) lista
     if(mysqli_insert_id($conn)){
-        header('location: usuarios_lista.php');
+        header('location: tipos_lista.php');
     }
+    $consulta_fk = "select * from tbtipos order by rotulo_tipo asc";
+    $lista_fk = $conn->query($consulta_fk);
+    $row_fk = $lista_fk->fetch_assoc();
+    $nlinhas = $lista_fk->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -28,43 +28,40 @@ if ($_POST){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/estilo.css">
-    <title>Usuários - Insere</title>
+    <title>Tipos - Insere</title>
 </head>
-<body >
-<?php include "menu_adm.php";?>
+<body>
+    <?php include "menu_adm.php";?>
     <main class="container">
         <div class="row">
-            <div class="col-xs-12 sm-offset-3 col-sm-6 col-md-offset-4 col-md-4">
+            <div class="col-xs-12 col-sm-offset-2 col-sm-6 col-md-8">
                 <h2 class="breadcrumb text-danger">
-                    <a href="usuarios_lista.php">
+                    <a href="tipos_lista.php">
                         <button class="btn btn-danger">
                             <span class="glyphicon glyphicon-chevron-left"></span>
                         </button>
                     </a>
-                    Inserindo Usuários
+                    Inserindo Tipos
                 </h2>
                 <div class="thumbnail">
                     <div class="alert alert-danger" role="alert">
-                        <form action="usuarios_insere.php" method="post" 
-                        name="form_usuario_insere" enctype="multipart/form-data"
-                        id="form_usuario_insere">
-                            <label for="login_usuario">Login:</label>
+                        <form action="tipos_insere.php" method="post" 
+                        name="form_tipos_insere" enctype="multipart/form-data"
+                        id="form_tipos_insere">
+                            <label for="id_tipo_produto">Tipo:</label>
                             <div class="input-group">
                                 <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+                                    <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
                                 </span>
-                                <input type="text" name="login_usuario" id="login_usuario" 
-                                class="form-control" placeholder="Digite o Login do usuario"
-                                maxlength="100" required>
+                                <input type="text" name="rotulo_tipo" class="form-control" placeholder="Digite o tipo do produto" required>
                             </div>
-                            <label for="nivel_usuario">Nível:</label>
+
+                            <label for="id_tipo_produto">Sigla:</label>
                             <div class="input-group">
                                 <span class="input-group-addon">
-                                    <span class="glyphicon glyphicon-cutlery" aria-hidden="true"></span>
+                                    <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
                                 </span>
-                                <input type="text" name="nivel_usuario" id="nivel_usuario" 
-                                class="form-control" placeholder="Digite o Nível do usuario"
-                                maxlength="100" required>
+                                <input type="text" name="sigla_tipo" class="form-control" placeholder="Digite a sigla do produto" required>
                             </div>
                             <br>
                             <input type="submit" name="enviar" id="enviar" class="btn btn-danger btn-block" value="Cadastrar">
@@ -74,6 +71,5 @@ if ($_POST){
             </div>
         </div>
     </main>
-</script>
 </body>
 </html>
