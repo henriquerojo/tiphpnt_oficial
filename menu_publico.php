319ed1cccbe2145439322acec51edc45bfc6c5af
henriquerojo <henriquerojo10@gmail.com>
2023-07-03 17:32:47 -0300

    arrumando identação moal menu_publico

--- menu_publico.php
+++ menu_publico.php
@@ -84,47 +84,47 @@ $rows_tipos = $lista_tipos->fetch_all();
             </nav> <!-- FIM NAV DIREITA -->
     <!-- FIM ABRE A BARRA DE NAVEGAÇÃO -->
 
-                                <!-- INÍCIO DO MODAL PARA FAZER RESERVA -->
-                                <div class="modal fade" id="modalReserva" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" tabindex="-1">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title" id="exampleModalLabel">Regras da Reserva</h4>
-                                            <button class="close" data-dismiss="modal" type="button" aria-label="Fechar">
-                                                &times;
-                                            </button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>
-                                            - Só uma pessoa é responsável por ser o titular da reserva;
-                                            <br>
-                                            - O titular da reserva e todos os demais acompanhantes terão 10% de desconto em bebidas;
-                                            <br>
-                                            - O número máximo de pessoas por mesa é de 8 pessoas;
-                                            <br>
-                                            - O número máximo de mesas por reserva são 2;,
-                                            <br>
-                                            - Somente um pedido de reserva por dia por CPF;
-                                            <br>
-                                            </p>
-                                            <h4><span class="nome text-danger"></span></h4>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <a href="index.php">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                                Cancelar
-                                            </button>
-                                            </a>
-                                            
-                                            <a href="pedido.php">
-                                            <button class="btn btn-primary" type="button">
-                                                Confirmar
-                                            </button>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- FIM DO MODAL PARA FAZER RESERVA -->
+    <!-- INÍCIO DO MODAL PARA FAZER RESERVA -->
+    <div class="modal fade" id="modalReserva" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" tabindex="-1">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="exampleModalLabel">Regras da Reserva</h4>
+                    <button class="close" data-dismiss="modal" type="button" aria-label="Fechar">
+                        &times;
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>
+                    - Só uma pessoa é responsável por ser o titular da reserva;
+                    <br>
+                    - O titular da reserva e todos os demais acompanhantes terão 10% de desconto em bebidas;
+                    <br>
+                    - O número máximo de pessoas por mesa é de 8 pessoas;
+                    <br>
+                    - O número máximo de mesas por reserva são 2;,
+                    <br>
+                    - Somente um pedido de reserva por dia por CPF;
+                    <br>
+                    </p>
+                    <h4><span class="nome text-danger"></span></h4>
+                </div>
+                <div class="modal-footer">
+                    <a href="index.php">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                        Cancelar
+                    </button>
+                    </a>
+                    
+                    <a href="pedido.php">
+                    <button class="btn btn-primary" type="button">
+                        Confirmar
+                    </button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- FIM DO MODAL PARA FAZER RESERVA -->
 </body>
 </html>
