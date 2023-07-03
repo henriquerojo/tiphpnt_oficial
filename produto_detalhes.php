3f4deb244099fdc9c0547bbe74dd15deec9f251a
henriquerojo <henriquerojo10@gmail.com>
2023-07-03 19:20:49 -0300

    estilizando detalhes produtos

--- produto_detalhes.php
+++ produto_detalhes.php
@@ -14,74 +14,73 @@ $row_detalhe = $produto_detalhe->fetch_assoc();
     <title>Detalhes do Produto</title>
     <link rel="stylesheet" href="css/estilo.css">
 </head>
+<style>
+    .center-column 
+    {
+        margin-left: auto;
+        margin-right: auto;
+        float: none !important;
+    }
+</style>
 <body class="fundofixo">
+    <!-- INÍCIO MENU PÚBLICO -->
     <header>
         <?php include "menu_publico.php";?>
     </header>
+    <!-- FIM MENU PÚBLICO -->
     <div class="container">
+        <!-- Top para voltar -->
         <h2 class="breadcrumb alert-danger">
             <a href="javascript:window.history.go(-1)" class="btn btn-danger">
                 <span class="glyphicon glyphicon-chevron-left"></span>
             </a>                
             Detalhes do Produto
         </h2>
-    </div>
-    <div class="card mb-3 breadcrumb">
-            <div class="row g-0">
-                <!-- imagem do produto -->
-                <div class="col-md-7">
+        <!-- Detalhe produtos -->
+        <div class="card mb-3 breadcrumb">
+            <div class="row g-0 text-center">
+                <!-- INÍCIO IMAGEM DO PRODUTO -->
+                <div class="col-md-7 center-column">
                     <img src="images/<?php echo $row_detalhe['imagem_produto'] ?>" class="img-fluid img-thumbnail rounded-start" alt="<?php ?>">
                 </div>
-                <!-- descrição do produto -->
-                <div class="col-md-5">
+                <!-- FIM IMAGEM DO PRODUTO -->
+                <div class="col-md-7 center-column">
                     <div class="card-body">
+                        <!-- INÍCIO NOME PRODUTO -->
                         <h3 class="text-danger text-center text-uppercase"> 
                             <strong><?php echo $row_detalhe['descri_produto'] ?></strong>
                         </h3>
-
-                        <p class="fs-3">
+                        <!-- FIM NOME PRODUTO -->
+                        <!-- INÍCIO TIPO DO PRODUTO -->
+                        <p class="fs-3 text-center">
                             <strong><?php echo $row_detalhe['rotulo_tipo'];?></strong>
                         </p>
-
+                        <!-- FIM TIPO DO PRODUTO -->
+                        <!-- INÍCIO RESUMO PRODUTO -->
                         <p class="">
                             <?php echo $row_detalhe['resumo_produto'];?>
                         </p>
-                        
-                        <div class="d-flex justify-content-end">
-                            <p><a href="" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Formas de pagamento</a></p>
+                        <!-- FIM RESUMO PRODUTO -->
+                        <div class="d-flex justify-content-end text-center">
 
                             <button class="btn btn-default disabled" role="button" style="cursor: default;">
                                 <?php echo "R$ ".number_format($row_detalhe['valor_produto'], 2, ',', '.');?>    
                             </button>
 
-                            <button class="btn btn-default btn-danger text-white text-uppercase" role="button">
+                            <button class="btn btn-default btn-success text-white text-uppercase" role="button">
                                 Comprar
                             </button>
-                        </div>
-                        <!-- modal pagamentos -->
-                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        ...
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="button" class="btn btn-primary">Understood</button>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <footer>
-            <?php include "rodape.php";?>
+        <!-- INÍCIO RODAPÉ -->
+        <footer class="panel-footer" style="background: none;">
+            <?php include 'rodape.php'; ?>
+            <a name="contato"></a>
         </footer>
+        <!-- FIM RODAPÉ -->
+    </div>
 </body>
 </html>
